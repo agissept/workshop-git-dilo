512ced1c7f61609a7aa6121f50d822a911884f3c
abuabdirohman4 <kakaabu354@yahoo.com>
2019-11-28 14:47:35 +0700

    Pertemuan 2

---
+++ abuabdirohman/code.js
@@ -0,0 +1,125 @@
+
+// let city = "Bandung";
+// let province = "Jawa Barat";
+// let no = 354;
+// let status = true;
+
+// let address = city + ", " + province;
+
+// console.log(city);
+
+// Contoh 2 
+// let age = 25;
+// let ageMin = age + 1;
+// let div = ages / 2;
+// let time = age + 2;
+
+// console.log{
+//     div;
+//     ageMin;
+//     time;
+// };
+
+// let isMale = true;
+// let isFemale = false;
+// if (isMale && isFemale) {
+//     console.log("Love")
+// } else if (isMale || isFemale) {
+//     console.log("Hate")
+// } else {
+//     console.log("Mboh")
+// };
+
+// let name = "Abu"
+// let getName = function () {
+//     console.log(name);
+
+//     let age = 20;
+// }
+
+// getName();
+
+// console.log(age);
+
+
+// function parameter
+
+// let add = function (x, y, z) {
+//     return x + y + z;
+// }
+
+// let result = add(3, 4, 5);
+// console.log(result);
+
+// let getScore = function (
+//     name = "Abu",
+//     score = 100
+// ) {
+//     return `${name} Score ${score}`
+// }
+// // let text = getScore('Lukman', 100);
+// let text = getScore(undefined, undefined);
+// console.log(text)
+
+// let firstName = "Abu";
+// let lastName = "Abdirohman";
+
+// let getAge = function() {
+//     return 25;
+// }
+
+// let fullName = `
+// Nama Saya
+// ${ firstName} ${lastName}
+// Usia saya ${getAge}`
+
+// console.log(fullName);
+
+// let myBook = {
+//     title: 'Nama Buku',
+//     penulis: 'Abu',
+//     halaman: '100'
+// }
+
+// // console.log(myBook.title);
+
+// // console.log(`
+// //     Judul Buku : ${myBook.title}
+// //     Pengarang : ${myBook.penulis}
+// // `)
+
+// let getBook = function (book) {
+//     comsole.log(`Judul Buku ${book.title}`)
+// }
+
+// (getBook(myBook));
+
+// let myAccount = {
+//     name: "John Die",
+//     expense: 0,
+//     income: 0
+// }
+
+// let addExpense = function (account, amount) {
+//     account.expense = account.expense + amount;
+//     console.log(account);
+// };
+
+// addExpense(myAccount, 500);
+// console.log(myAccount);
+
+// let name = "Abu Abdirohman"
+
+// console.log(name.length);
+// console.log(name.toUpperCase);
+// console.log(name.toLowerCase);
+
+// let activities = ['Ngaji', 'Shalat', 'Sambung']
+// activities.forEach(value, index){
+//     `
+//     Value : ${activities.value}
+//     Index : ${activiti es.index
+// }
+// `}
+
+// console.log(activities);
